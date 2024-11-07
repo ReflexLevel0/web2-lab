f0804ff357c5a29d9eb95bf46ee8ec13b8ec0a90
ReflexLevel0 <dominikdejanovic2@gmail.com>
2024-11-07 19:34:48 +0100

    Split authentication and injection into separate components

--- lab02/client/src/App.vue
+++ lab02/client/src/App.vue
@@ -1,78 +1,19 @@
 <script>
-import alasql from "alasql";
+import Injection from "./components/Injection.vue";
+import Authentication from "./components/Authentication.vue";
 
 export default {
-  data() {
-    return {
-      people: [],
-      filteredPeople: [],
-      injectionEnabled: false,
-      name_input_text: "",
-    };
-  },
-  mounted() {
-    //alasql("DROP TABLE IF EXISTS person");
-    alasql(
-      "CREATE TABLE IF NOT EXISTS person(id SERIAL PRIMARY KEY, first_name VARCHAR(64), last_name VARCHAR(64))",
-    );
-    if (alasql.tables.person.data.length == 0) {
-      alasql(
-        "INSERT INTO person(first_name, last_name) VALUES ('Pero', 'Perić'),('Ante', 'Antić'),('Pero', 'Perković')",
-      );
-    }
-    //alasql("DROP VIEW IF EXISTS personView");
-    alasql(
-      'CREATE VIEW IF NOT EXISTS personView AS SELECT person.*, first_name || " " || last_name as full_name FROM person',
-    );
-    this.$data.people = alasql("SELECT * FROM personView");
-    this.$data.filteredPeople = this.$data.people;
-  },
-  methods: {
-    filterPeople() {
-      let sql_string = "";
-      let filtered = [];
-      let name = this.$data.name_input_text;
-      if (this.$data.injectionEnabled) {
-        sql_string =
-          'SELECT * FROM personView WHERE full_name LIKE "%' + name + '%"';
-        filtered = alasql(sql_string);
-      } else {
-        sql_string = "SELECT * FROM personView WHERE full_name LIKE ?";
-        filtered = alasql(sql_string, ["%" + name + "%"]);
-      }
-      this.$data.filteredPeople = filtered;
-    },
+  components: {
+    Injection,
+    Authentication,
   },
 };
 </script>
 
 <template>
   <div>
-    <label for="sql-injection-input">SQL injection</label>
-    <input
-      id="sql-injection-input"
-      type="checkbox"
-      @input="
-        (input) => {
-          this.$data.injectionEnabled = input.srcElement.checked;
-          this.filterPeople();
-        }
-      "
-    />
-
-    <br />
-
-    <label for="name-input">Name search: </label>
-    <input
-      id="name-input"
-      type="text"
-      v-model="name_input_text"
-      @input="() => this.filterPeople()"
-    />
-
-    <div :key="person.id" v-for="person in this.$data.filteredPeople">
-      <div>{{ person }}</div>
-    </div>
+    <Injection />
+    <Authentication />
   </div>
 </template>
 
---
+++ lab02/client/src/components/Authentication.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Broken Authentication</h2>
+    <form>
+      <label for="username-input">Username: </label>
+      <input id="username-input" type="text" />
+      <br />
+      <label for="password-input">Password: </label>
+      <input id="password-input" type="text" />
+      <br />
+      <button type="submit">Log in</button>
+    </form>
+  </div>
+</template>
